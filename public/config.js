41ac3e4ad0fb979dac9c41035f823c0ff57d9dca
jakub_banasiak <soccerkid.bany@gmail.com>
2020-02-02 20:30:17 +0100

    fix config file

--- public/config.js
+++ public/config.js
@@ -1,3 +1,5 @@
-    const apiKey = "AIzaSyDV9NSCzru5_SgdM_6Ln7OkOC-Wyix4JBM";
-    const messagingSenderId = "682834449377";
-    const appId = "1:682834449377:web:f92677b59a60f57f8b78a0";
+const config = {
+    apiKey: "AIzaSyDV9NSCzru5_SgdM_6Ln7OkOC-Wyix4JBM",
+    messagingSenderId: "682834449377",
+    appId: "1:682834449377:web:f92677b59a60f57f8b78a0",
+};
