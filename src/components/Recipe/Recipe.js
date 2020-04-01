c920dda22b46022cf0ff1453ae73c7dce10ebe2b
jakub_banasiak <soccerkid.bany@gmail.com>
2020-04-01 20:20:15 +0200

    adjust styles and add first recipe

--- src/components/Recipe/Recipe.js
+++ src/components/Recipe/Recipe.js
@@ -20,26 +20,44 @@ const recipeMock = {
     name: "Domowa pizza",
     ingredients: [
         {
-            name: "jabłka",
-            quantity: "2",
-            unit: "kg",
+            name: "świeże drożdże",
+            quantity: "25",
+            unit: "g",
         },
         {
-            name: "mąka",
-            quantity: "1",
-            unit: "kg",
+            name: "ciepła woda",
+            quantity: "150",
+            unit: "ml",
         },
         {
-            name: "jajka",
-            quantity: "2",
-            unit: "szt.",
+            name: "cukier",
+            quantity: "5",
+            unit: "g",
+        },
+        {
+            name: "mąka pszenna",
+            quantity: "250",
+            unit: "g",
+        },
+        {
+            name: "sól",
+            quantity: "5",
+            unit: "g",
+        },
+        {
+            name: "oliwa",
+            quantity: "5",
+            unit: "ml",
         },
     ],
     preparation: {
-        1: "obrać jajka i wrzucić do wody",
-        2: "mąkę rozsypać na podłodze",
-        3: "zjeść jabłka (tylo bez pestek bo może drzewo w brzuchu wyrosnąć)",
-        4: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+        1: "Świeże drożdże ocieplić w temperaturze pokojowej. Przygotować rozczyn: drożdże rozpuścić w ciepłej wodzie, dodać 2 łyżki mąki oraz cukier, dokładnie wymieszać i odstawić na ok. 10 minut do wyrośnięcia (rozczyn ze świeżych drożdży zwiększa objętość o ok. 3 razy - jeśli tak się nie stanie proces przygotowania rozczynu trzeba powtórzyć od nowa, natomiast rozczyn z drożdży instant może się tylko trochę spienić).",
+        2: "Mąkę przesiać do miski, wymieszać z solą, zrobić wgłębienie w środku i wlać w nie rozczyn. Sukcesywnie zagarniać łyżką mąkę do środka i przez 2 - 3 minuty mieszać składniki, pod koniec dodając jeszcze oliwę.",
+        3: "Połączone składniki wyłożyć na stolnicę oprószoną mąką. Wyrabiać przez ok. 15 minut aż ciasto będzie elastyczne i gładkie (ciasto można też zagnieść mikserem planetarnym)",
+        4: "Wyrobione ciasto włożyć do dużej miski, przykryć ściereczką i odstawić na ok. 1 godzinę do wyrośnięcia",
+        5: "Wyrośnięte ciasto wyjąć na stolnicę i chwilę pozagniatać. Podzielić na 2 części, uformować z nich kulki i odłożyć na ok. 7 minut pod ściereczką.",
+        6: 'Blaszki (tortownice) posmarować oliwą. Włożyć na środek kulkę ciasta, delikatnie spłaszczyć i rozciągać, rozprowadzając palcami po całej powierzchni dna, zaczynając od środka i zostawiając niewielki "wałeczek" na brzegu (zob. zdjęcia poniżej). UWAGA: najlepiej robić to kilkoma etapami, ciasto na początku sprężynuje i "cofa się" ale jeśli odczekamy chwilę będziemy mogli je dalej rozciągać.',
+        7: "Wyłożyć cienką warstwę SOSU POMIDOROWEGO, ser* oraz ulubione dodatki. Odczekać ok. 15 minut aż ciasto podrośnie, następnie piec w maksymalnie nagrzanym piekarniku (min. 250 st. C) przez ok. 10 minut.",
     },
 };
 
