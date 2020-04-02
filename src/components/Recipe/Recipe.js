c8274c5e07e1d6a73de94488616ff1d79f930fb7
jakub_banasiak <soccerkid.bany@gmail.com>
2020-04-02 23:58:05 +0200

    code refactory and add clicked ingredient to state

--- src/components/Recipe/Recipe.js
+++ src/components/Recipe/Recipe.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { useParams } from "react-router-dom";
 
 import RecipeNavbar from "../RecipeNavbar/RecipeNavbar";
@@ -7,62 +7,17 @@ import RecipeContentHeader from "../RecipeContentHeader/RecipeContetHeader";
 import { recipeMock } from "./pizza1";
 
 import "./Recipe.css";
+import RecipeContentMain from "../RecipeContentMain/RecipeContentMain";
 
 const Recipe = () => {
-    const [ingredientsTab, changeTab] = useState(true);
     let { id } = useParams();
+
     return (
         <div className="recipe">
             <RecipeNavbar />
             <div className="recipe-content">
                 <RecipeContentHeader data={recipeMock.generalData} />
-                <div className="recipe-content-main">
-                    <button
-                        type="button"
-                        onClick={() => changeTab(true)}
-                        className="recipe-main-button"
-                    >
-                        Składniki
-                    </button>
-                    <button
-                        type="button"
-                        onClick={() => changeTab(false)}
-                        className="recipe-main-button"
-                    >
-                        Przepis
-                    </button>
-                    <div className="recipe-content-main--info">
-                        Dotknij składnik, który chcesz wykreśilić bo już go masz
-                    </div>
-                    {ingredientsTab ? (
-                        <ul className="ingredients-list">
-                            {recipeMock.ingredients.map((elem) => (
-                                <li
-                                    key={elem.name}
-                                    className="ingredients-list-item"
-                                >{`${elem.name} ${elem.quantity} ${elem.unit}`}</li>
-                            ))}
-                        </ul>
-                    ) : (
-                        <ul className="ingredients-list">
-                            {Object.keys(recipeMock.preparation).map((elem) => {
-                                return (
-                                    <li
-                                        key={elem}
-                                        className="preparation-list-item"
-                                    >
-                                        <span className="preparation-list-item__header">
-                                            Krok {elem}
-                                        </span>
-                                        <div className="preparation-list-item__content">
-                                            {recipeMock.preparation[elem]}
-                                        </div>
-                                    </li>
-                                );
-                            })}
-                        </ul>
-                    )}
-                </div>
+                <RecipeContentMain data={recipeMock.recipeMain} />
             </div>
         </div>
     );
